c635bf1b7ebadd53aa63ddad425d03729b12bb7e
dextergb <dextergb@rogers.com>
2021-06-02 17:00:36 +0000

    implemented name and connect

--- client.js
+++ client.js
@@ -4,19 +4,23 @@ const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "135.23.222.131",
-    port: 50542}, () => {
-      console.log("Connection established!");
+    port: 50542
   });
   conn.on("connect", () => {
     // code that does something when the connection is first established
     console.log("it's connected")
   });
+
   conn.on("data", (data) => {
     console.log(data);
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  const name = 'DGB';
+  conn.write(`Name: ${name}`);
+
+
 
   return conn;
 };
